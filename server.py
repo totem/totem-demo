f1c1c37cfa5d2b2b0faa23246899e5625764d27c
sukrit007 <sukrit007@gmail.com>
2015-08-13 09:32:10 -0700

    Timeout demo

--- server.py
+++ server.py
@@ -1,4 +1,17 @@
 from demo.views import app
+import time
+import signal
+import sys
+
+
+def sigterm_handler(_signo, _stack_frame):
+    # Raises SystemExit(0):
+    print("Handling SIGTERM")
+    time.sleep(150)
+    print("Handling SIGTERM: Exit")
+    sys.exit(0)
+
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True, port=8080, host='0.0.0.0')
+    signal.signal(signal.SIGTERM, sigterm_handler)
+    app.run(debug=False, threaded=True, port=8080, host='0.0.0.0')
