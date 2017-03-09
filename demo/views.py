c43b2879fc6d31623459fbf9df4ba5517858cfd6
Sukrit Khera <sukrit007@gmail.com>
2017-03-09 13:44:18 -0700

    Update views.py

--- demo/views.py
+++ demo/views.py
@@ -9,7 +9,7 @@ app = flask.Flask(__name__)
 @app.route('/totem-demo')
 def hello_world():
     return flask.jsonify({
-        'message': 'Hello World v5!',
+        'message': 'Hello World v6!',
         'python': sys.version,
         'headers': str(request.headers)
     })
