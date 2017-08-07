d6db03243eeaf6abd25fab5edf48a1489b367e65
Sukrit Khera <sukrit.khera@meltmedia.com>
2017-08-07 14:01:42 -0700

    Add example for xframe options

--- demo/views.py
+++ demo/views.py
@@ -7,8 +7,10 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return flask.jsonify({
+    resp = flask.jsonify({
         'message': 'Hello World!',
         'python': sys.version,
         'headers': str(request.headers)
     })
+    resp.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    return resp
